45425c0bc57a8af15ae17008f663a04d0303e301
Nay Thu Khant <naythukhant644@gmail.com>
2023-02-20 17:43:50 +0630

    initial commit

--- Formula/onenex.rb
+++ Formula/onenex.rb
@@ -2,9 +2,9 @@ class Onenex < Formula
   desc "Installer to install starter kits from https://github.com/onenextech"
   homepage "https://github.com/onenextech/installer"
   url "https://github.com/onenextech/installer/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "9cc2e40b5336eff2afbe6be8d7bf2ae0f29f19976d8a7517d974ba7b7efbb90b"
+  sha256 "2c76597215842f7a331702721c62c144ee8d3f1215655eb131a8a5b958b7bf8c"
 
   def install
-    bin.install "onenex.sh"
+    bin.install "onenex-installer.sh"
   end
 end
