5dbd89e195040f5a36038f4b72fc461155236e26
Nay Thu Khant <naythukhant644@gmail.com>
2023-02-20 17:55:48 +0630

    initial commit

--- onenex-installer.rb
+++ onenex-installer.rb
@@ -1,7 +1,7 @@
-class Onenex < Formula
+class OnenexInstaller < Formula
   desc "Installer to install starter kits from https://github.com/onenextech"
-  homepage "https://github.com/onenextech/installer"
-  url "https://github.com/onenextech/installer/archive/refs/tags/v1.0.0.tar.gz"
+  homepage "https://github.com/onenextech/onenex-installer"
+  url "https://github.com/onenextech/onenex-installer/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "2c76597215842f7a331702721c62c144ee8d3f1215655eb131a8a5b958b7bf8c"
 
   def install
